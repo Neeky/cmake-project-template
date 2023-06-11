d431d6721532278e71718013f9a5a415d6039451
Neeky <1721900707@qq.com>
2023-06-11 19:59:34 +0800

    init-cmake-project-template

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "tools/math.h"
+
+int main(int argc, char **argv)
+{
+    using namespace std;
+    cout << "this is cmake-project-template 100 + 100 = " << add(100, 100) << " ." << endl;
+    return 0;
+}
